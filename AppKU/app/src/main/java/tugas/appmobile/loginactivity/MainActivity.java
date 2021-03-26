e6542223892e42eb5e8db2e986b32a0b85ed08d6
irhaf03 <ririfah11@gmail.com>
2021-03-26 16:14:46 +0700

    Login Activity

---
+++ AppKU/app/src/main/java/tugas/appmobile/loginactivity/MainActivity.java
@@ -0,0 +1,14 @@
+package tugas.appmobile.loginactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
